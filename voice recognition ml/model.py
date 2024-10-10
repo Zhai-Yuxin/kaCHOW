217a02d7282f65a263d7bfbd690637b465f9a12c
Daryl <darylchen46@gmail.com>
2024-10-10 11:34:33 +0800

    added some test code

--- voice recognition ml/model.py
+++ voice recognition ml/model.py
@@ -6,6 +6,37 @@ from torch.utils.data import TensorDataset, DataLoader
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
 
+# Audio Data Pre-processing
+
+import librosa.display, os
+import matplotlib.pyplot as plt
+
+def create_spectrogram(audio_file, image_file):
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+    fig.subplots_adjust(left=0, right=1, bottom=0, top=1)
+
+    y, sr = librosa.load(audio_file)
+    ms = librosa.feature.melspectrogram(y=y, sr=sr)
+    log_ms = librosa.power_to_db(ms, ref=np.max)
+    librosa.display.specshow(log_ms, sr=sr)
+
+    fig.savefig(image_file)
+    plt.close(fig)
+    
+def create_pngs_from_wavs(input_path, output_path):
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+
+    dir = os.listdir(input_path)
+
+    for i, file in enumerate(dir):
+        input_file = os.path.join(input_path, file)
+        output_file = os.path.join(output_path, file.replace('.wav', '.png'))
+        create_spectrogram(input_file, output_file)
+
+# CNN Model
+
 batch_size = 64
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
