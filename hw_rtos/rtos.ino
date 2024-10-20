fa6a88f76dc7fe276fc455d0018abac3507a2798
Zhai Yuxin <e0772744@u.nus.edu>
2024-10-21 00:20:16 +0800

    add in for avoidance, motor, lcd, servo

--- hw_rtos/rtos.ino
+++ hw_rtos/rtos.ino
@@ -1,5 +1,6 @@
-#include "Cdrv8833.h"
 #include "pitches.h"
+#include <LiquidCrystal_I2C.h>
+#include <ESP32Servo.h>
 
 #define LED1 5
 #define LED2 5
@@ -13,19 +14,28 @@
 #define MOTOR1_IN2 22
 #define MOTOR2_IN3 23
 #define MOTOR2_IN4 24
+#define MOTOR1_CHANNEL_A 0  // PWM channel for forward direction
+#define MOTOR1_CHANNEL_B 1  // PWM channel for reverse direction
+#define MOTOR2_CHANNEL_A 2  // PWM channel for forward direction
+#define MOTOR2_CHANNEL_B 3  // PWM channel for reverse direction
+#define MOTOR_FREQ 5000     // PWM frequency
+#define MOTOR_RESOLUTION 8  // PWM resolution (8-bit: 0-255)
 
-#define LCD 5
-#define SERVO 5  // waving small flag constantly when on?
+// connect LCD_SDA 21
+// connect LCD_SCL 22
+#define SERVO 5  // waving small flag when botton pressed on web?
 
 // flags
 volatile int control = 0;
 volatile bool front_obstacle = 0;
 volatile bool back_obstacle = 0;
+volatile bool wave = 0;
 
-#define CHANNEL 0 // there are 16 unique PWM channels (0..15)
-#define SWAP false // swap motor rotation direction
-Cdrv8833 myMotor1;
-Cdrv8833 myMotor2;
+LiquidCrystal_I2C lcd(0x27,16,2);
+
+Servo myservo;
+volatile int pos = 0;
+volatile int change = 1;
 
 int melody[] = {
   NOTE_E5, NOTE_E5, NOTE_E5,
@@ -49,91 +59,87 @@ int noteDurations[] = {
   4, 4
 };
 
-void left() {
-    digitalWrite(LED1, HIGH); 
-    digitalWrite(LED2, HIGH); 
-    digitalWrite(LED3, LOW); 
-    digitalWrite(LED4, LOW); 
-    digitalWrite(LED5, LOW); 
-    vTaskDelay(500 / portTICK_PERIOD_MS); 
-    digitalWrite(LED1, LOW); 
-    digitalWrite(LED2, LOW); 
-    vTaskDelay(500 / portTICK_PERIOD_MS); 
-} // control = 3
-
-void right() {
-    digitalWrite(LED1, LOW); 
-    digitalWrite(LED2, LOW); 
-    digitalWrite(LED3, LOW); 
-    digitalWrite(LED4, HIGH); 
-    digitalWrite(LED5, HIGH); 
-    vTaskDelay(500 / portTICK_PERIOD_MS); 
-    digitalWrite(LED4, LOW); 
-    digitalWrite(LED5, LOW); 
-    vTaskDelay(500 / portTICK_PERIOD_MS); 
-} // control = 4
-
-void straight() {
-    digitalWrite(LED1, HIGH); 
-    digitalWrite(LED2, HIGH);
-    digitalWrite(LED3, HIGH);
-    digitalWrite(LED4, HIGH);
-    digitalWrite(LED5, HIGH);
-} // control = 1
-
-void reverse() {
-    digitalWrite(LED1, LOW); 
-    digitalWrite(LED2, LOW); 
-    digitalWrite(LED3, LOW); 
-    digitalWrite(LED4, LOW); 
-    digitalWrite(LED5, LOW); 
-} // control = 2
-
-void stop() {
-    digitalWrite(LED1, HIGH);
-    digitalWrite(LED2, HIGH);
-    digitalWrite(LED3, HIGH);
-    digitalWrite(LED4, HIGH);
-    digitalWrite(LED5, HIGH);
-    vTaskDelay(1000 / portTICK_PERIOD_MS); // block for 1000ms
-    digitalWrite(LED1, LOW); 
-    digitalWrite(LED2, LOW); 
-    digitalWrite(LED3, LOW); 
-    digitalWrite(LED4, LOW); 
-    digitalWrite(LED5, LOW); 
-    vTaskDelay(1000 / portTICK_PERIOD_MS);
-}  // control = 0
-
 void led(void * pvParameters) {
-    if (control == 0) {
-        stop();
-    } else if (control == 1) {
-        straight();
-    } else if (control == 2) {
-        reverse();
-    } else if (control == 3) {
-        left();
-    } else if (control == 4) {
-        right();
+    if (control == 0) {  // stop
+        digitalWrite(LED1, HIGH);
+        digitalWrite(LED2, HIGH);
+        digitalWrite(LED3, HIGH);
+        digitalWrite(LED4, HIGH);
+        digitalWrite(LED5, HIGH);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        digitalWrite(LED1, LOW); 
+        digitalWrite(LED2, LOW); 
+        digitalWrite(LED3, LOW); 
+        digitalWrite(LED4, LOW); 
+        digitalWrite(LED5, LOW); 
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    } else if (control == 1) {  // straight
+        digitalWrite(LED1, HIGH); 
+        digitalWrite(LED2, HIGH);
+        digitalWrite(LED3, HIGH);
+        digitalWrite(LED4, HIGH);
+        digitalWrite(LED5, HIGH);
+    } else if (control == 2) {  // reverse
+        digitalWrite(LED1, LOW); 
+        digitalWrite(LED2, LOW); 
+        digitalWrite(LED3, LOW); 
+        digitalWrite(LED4, LOW); 
+        digitalWrite(LED5, LOW); 
+    } else if (control == 3) {  // left
+        digitalWrite(LED1, HIGH); 
+        digitalWrite(LED2, HIGH); 
+        digitalWrite(LED3, LOW); 
+        digitalWrite(LED4, LOW); 
+        digitalWrite(LED5, LOW); 
+        vTaskDelay(500 / portTICK_PERIOD_MS); 
+        digitalWrite(LED1, LOW); 
+        digitalWrite(LED2, LOW); 
+        vTaskDelay(500 / portTICK_PERIOD_MS); 
+    } else if (control == 4) {  // right
+        digitalWrite(LED1, LOW); 
+        digitalWrite(LED2, LOW); 
+        digitalWrite(LED3, LOW); 
+        digitalWrite(LED4, HIGH); 
+        digitalWrite(LED5, HIGH); 
+        vTaskDelay(500 / portTICK_PERIOD_MS); 
+        digitalWrite(LED4, LOW); 
+        digitalWrite(LED5, LOW); 
+        vTaskDelay(500 / portTICK_PERIOD_MS); 
     }
 }
 
-void motor(void * pvParameters) {
+// needs to be updated ...
+void motor(void * pvParameters) {  // library removed, with pwm to write directly instead
     if ((control == 0) || (front_obstacle == 1) || (back_obstacle == 1)) {
-        myMotor1.stop();
-        myMotor2.stop();
+        ledcWriteChannel(MOTOR1_CHANNEL_A, 0);
+        ledcWriteChannel(MOTOR1_CHANNEL_B, 0);
+        ledcWriteChannel(MOTOR2_CHANNEL_A, 0);
+        ledcWriteChannel(MOTOR2_CHANNEL_B, 0);
+        vTaskDelay(2000 / portTICK_PERIOD_MS); 
     } else if (control == 1) {
-        myMotor1.move(50); //rotation power -100..100
-        myMotor2.move(50);
+        ledcWriteChannel(MOTOR1_CHANNEL_A, 200);
+        ledcWriteChannel(MOTOR1_CHANNEL_B, 0);
+        ledcWriteChannel(MOTOR2_CHANNEL_A, 200);
+        ledcWriteChannel(MOTOR2_CHANNEL_B, 0);
+        vTaskDelay(2000 / portTICK_PERIOD_MS); 
     } else if (control == 2) {
-        myMotor1.move(-50);
-        myMotor2.move(-50);
+        ledcWriteChannel(MOTOR1_CHANNEL_A, 0);
+        ledcWriteChannel(MOTOR1_CHANNEL_B, 200);
+        ledcWriteChannel(MOTOR2_CHANNEL_A, 0);
+        ledcWriteChannel(MOTOR2_CHANNEL_B, 200);
+        vTaskDelay(2000 / portTICK_PERIOD_MS); 
     } else if (control == 3) {
-        myMotor1.move(20);
-        myMotor2.move(40);
+        ledcWriteChannel(MOTOR1_CHANNEL_A, 100);
+        ledcWriteChannel(MOTOR1_CHANNEL_B, 0);
+        ledcWriteChannel(MOTOR2_CHANNEL_A, 150);
+        ledcWriteChannel(MOTOR2_CHANNEL_B, 0);
+        vTaskDelay(1000 / portTICK_PERIOD_MS); 
     } else if (control == 4) {
-        myMotor1.move(40);
-        myMotor2.move(20);
+        ledcWriteChannel(MOTOR1_CHANNEL_A, 150);
+        ledcWriteChannel(MOTOR1_CHANNEL_B, 0);
+        ledcWriteChannel(MOTOR2_CHANNEL_A, 100);
+        ledcWriteChannel(MOTOR2_CHANNEL_B, 0);
+        vTaskDelay(1000 / portTICK_PERIOD_MS); 
     }
 }
 
@@ -150,20 +156,81 @@ void buzz() {
     }
 }
 
+void avoidance() {
+    bool obstacle = digitalRead(AVOIDANCE1);
+    if (!obstacle) {
+        front_obstacle = 1;
+    } else {
+        front_obstacle = 0;
+    }
+    obstacle = digitalRead(AVOIDANCE2);
+    if (!obstacle) {
+        back_obstacle = 1;
+    } else {
+        back_obstacle = 0;
+    }
+    vTaskDelay(500 / portTICK_PERIOD_MS); 
+}
+
+void display() {
+    lcd.setCursor(0, 0);
+    lcd.print("Detected:");
+    lcd.setCursor(0,1);
+    if (control == 0) {
+        lcd.print("stop");
+    } else if (control == 1) {
+        lcd.print("straight");
+    } else if (control == 2) {
+        lcd.print("reverse");
+    } else if (control == 3) {
+        lcd.print("left");
+    } else if (control == 4) {
+        lcd.print("right");
+    }
+    vTaskDelay(1000 / portTICK_PERIOD_MS); 
+    lcd.clear();
+}
+
+void servo() {
+    if (wave) {
+        myservo.write(pos);
+        change = (pos<0)? 1 : ((pos>180)? -1: change);
+        pos += change;
+        vTaskDelay(5 / portTICK_PERIOD_MS);
+    } else {
+        vTaskDelay(1000 / portTICK_PERIOD_MS); 
+    }
+}
+
 void setup() {
-  pinMode(LED1, OUTPUT);
-  pinMode(LED2, OUTPUT);
-  pinMode(LED3, OUTPUT);
-  pinMode(LED4, OUTPUT);
-  pinMode(LED5, OUTPUT);
-  // pinMode(LED_BUILTIN, OUTPUT)
-  xTaskCreate(led, "led", 2048, NULL, 1, NULL);
+    pinMode(LED1, OUTPUT);
+    pinMode(LED2, OUTPUT);
+    pinMode(LED3, OUTPUT);
+    pinMode(LED4, OUTPUT);
+    pinMode(LED5, OUTPUT);
+    // pinMode(LED_BUILTIN, OUTPUT)
+    xTaskCreate(led, "led", 2048, NULL, 1, NULL);
 
-  myMotor1.init(MOTOR1_IN1, MOTOR1_IN2, CHANNEL, SWAP);
-  myMotor2.init(MOTOR2_IN3, MOTOR2_IN4, CHANNEL, SWAP);
-//   xTaskCreate(motor, "motor", 2048, NULL, 1, NULL);  -- be handled as interrupt
+    ledcAttachChannel(MOTOR1_IN1, MOTOR_FREQ, MOTOR_RESOLUTION, MOTOR1_CHANNEL_A);
+    ledcAttachChannel(MOTOR1_IN1, MOTOR_FREQ, MOTOR_RESOLUTION, MOTOR1_CHANNEL_B);
+    ledcAttachChannel(MOTOR2_IN4, MOTOR_FREQ, MOTOR_RESOLUTION, MOTOR2_CHANNEL_A);
+    ledcAttachChannel(MOTOR2_IN4, MOTOR_FREQ, MOTOR_RESOLUTION, MOTOR2_CHANNEL_B);
+    xTaskCreate(motor, "motor", 2048, NULL, 1, NULL);  // be handled as interrupt? no
 
-  xTaskCreate(buzz, "buzz", 2048, NULL, 1, NULL);
+    pinMode(BUZZER, OUTPUT);
+    xTaskCreate(buzz, "buzz", 2048, NULL, 1, NULL);
+
+    pinMode(AVOIDANCE1, INPUT);
+    pinMode(AVOIDANCE2, INPUT);
+    xTaskCreate(avoidance, "avoidance", 2048, NULL, 1, NULL);
+
+    lcd.init();
+    lcd.backlight();
+    xTaskCreate(display, "display", 2048, NULL, 1, NULL);
+
+	myservo.setPeriodHertz(50);    // standard 50 hz servo
+	myservo.attach(SERVO, 100, 4000);
+    xTaskCreate(servo, "servo", 2048, NULL, 1, NULL);
 }
 
 void loop() {
