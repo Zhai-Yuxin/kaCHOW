9df19d72b3b6cd5c7ba6e1015ba505b4f06e0fc8
Daryl <darylchen46@gmail.com>
2024-10-27 01:18:22 +0800

    change record timer to 4 seconds

--- microphone/inmp441_to_mqtt_sleep/inmp441_to_mqtt_sleep.ino
+++ microphone/inmp441_to_mqtt_sleep/inmp441_to_mqtt_sleep.ino
@@ -11,7 +11,7 @@
 #define I2S_SAMPLE_RATE   (16000)
 #define I2S_SAMPLE_BITS   (16)
 #define I2S_READ_LEN      (16 * 1024)
-#define RECORD_TIME       (1) //Seconds
+#define RECORD_TIME       (4) //Seconds
 #define I2S_CHANNEL_NUM   (1)
 #define FLASH_RECORD_SIZE (I2S_CHANNEL_NUM * I2S_SAMPLE_RATE * I2S_SAMPLE_BITS / 8 * RECORD_TIME)
 #define uS_TO_S_FACTOR 1000000ULL /* Conversion factor for micro seconds to seconds */
