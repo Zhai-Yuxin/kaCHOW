31072e0993768fda07afb563a1c5b40e7105cc89
Daryl <darylchen46@gmail.com>
2024-10-30 02:33:28 +0800

    refactor code

--- microphone/inmp441_to_mqtt_sleep_nus_stu/inmp441_to_mqtt_sleep_nus_stu.ino
+++ microphone/inmp441_to_mqtt_sleep_nus_stu/inmp441_to_mqtt_sleep_nus_stu.ino
@@ -38,7 +38,6 @@ const char* mqtt_topic = "voice/wav";
 
 File file;
 const char filename[] = "/recording.wav";
-const int headerSize = 44;
 
 void print_wakeup_reason() {
   esp_sleep_wakeup_cause_t wakeup_reason;
@@ -47,9 +46,9 @@ void print_wakeup_reason() {
 
   switch (wakeup_reason) {
     case ESP_SLEEP_WAKEUP_EXT0:     Serial.println("Wakeup caused by external signal using RTC_IO"); break;
-    case ESP_SLEEP_WAKEUP_EXT1:     Serial.println("Wakeup caused by external signal using RTC_CNTL"); break;
-    case ESP_SLEEP_WAKEUP_TIMER:    Serial.println("Wakeup caused by timer"); break;
-    case ESP_SLEEP_WAKEUP_TOUCHPAD: Serial.println("Wakeup caused by touchpad"); break;
+    case ESP_SLEEP_WAKEUP_EXT1:     Serial.println("Woken up by sound sensor"); break;
+    case ESP_SLEEP_WAKEUP_TIMER:    Serial.println("Woken up by timer"); break;
+    case ESP_SLEEP_WAKEUP_TOUCHPAD: Serial.println("Woken up by touch"); break;
     case ESP_SLEEP_WAKEUP_ULP:      Serial.println("Wakeup caused by ULP program"); break;
     default:                        Serial.printf("Wakeup was not caused by deep sleep: %d\n", wakeup_reason); break;
   }
@@ -67,7 +66,7 @@ void setup() {
   if (esp_sleep_get_wakeup_cause() != ESP_SLEEP_WAKEUP_TIMER) {
     SPIFFSInit();
     i2sInit();
-    i2s_adc(NULL);
+    i2s_adc();
   
     // Connect to wifi
     setup_wifi();
@@ -91,12 +90,8 @@ void setup() {
   esp_sleep_enable_touchpad_wakeup();;
 
   // Wake up by sound
-  pinMode(SOUNDPIN, INPUT);
-  Serial.println(); 
+  pinMode(SOUNDPIN, INPUT); 
   esp_sleep_enable_ext1_wakeup(1ULL << SOUNDPIN, ESP_EXT1_WAKEUP_ANY_HIGH);
-  if (esp_sleep_get_wakeup_cause() == ESP_SLEEP_WAKEUP_EXT1) {
-        Serial.println("Woken up by KY-038 sound sensor!");
-  }
 
   Serial.flush();
   esp_deep_sleep_start();
@@ -202,7 +197,7 @@ void i2s_adc_data_scale(uint8_t * d_buff, uint8_t* s_buff, uint32_t len) {
     }
 }
 
-void i2s_adc(void *arg) {
+void i2s_adc() {
     
     int i2s_read_len = I2S_READ_LEN;
     int flash_wr_size = 0;
@@ -219,7 +214,6 @@ void i2s_adc(void *arg) {
     while (flash_wr_size < FLASH_RECORD_SIZE) {
         //read data from I2S bus, in this case, from ADC.
         i2s_read(I2S_PORT, (void*) i2s_read_buff, i2s_read_len, &bytes_read, portMAX_DELAY);
-        //example_disp_buf((uint8_t*) i2s_read_buff, 64);
         //save original data from I2S(ADC) into flash.
         i2s_adc_data_scale(flash_write_buff, (uint8_t*)i2s_read_buff, i2s_read_len);
         file.write((const byte*) flash_write_buff, i2s_read_len);
@@ -237,66 +231,6 @@ void i2s_adc(void *arg) {
     
     listSPIFFS();    
 }
-
-void example_disp_buf(uint8_t* buf, int length) {
-    printf("======\n");
-    for (int i = 0; i < length; i++) {
-        printf("%02x ", buf[i]);
-        if ((i + 1) % 8 == 0) {
-            printf("\n");
-        }
-    }
-    printf("======\n");
-}
-
-void wavHeader(byte* header, int wavSize) {
-  header[0] = 'R';
-  header[1] = 'I';
-  header[2] = 'F';
-  header[3] = 'F';
-  unsigned int fileSize = wavSize + headerSize - 8;
-  header[4] = (byte)(fileSize & 0xFF);
-  header[5] = (byte)((fileSize >> 8) & 0xFF);
-  header[6] = (byte)((fileSize >> 16) & 0xFF);
-  header[7] = (byte)((fileSize >> 24) & 0xFF);
-  header[8] = 'W';
-  header[9] = 'A';
-  header[10] = 'V';
-  header[11] = 'E';
-  header[12] = 'f';
-  header[13] = 'm';
-  header[14] = 't';
-  header[15] = ' ';
-  header[16] = 0x10;
-  header[17] = 0x00;
-  header[18] = 0x00;
-  header[19] = 0x00;
-  header[20] = 0x01;
-  header[21] = 0x00;
-  header[22] = 0x01;
-  header[23] = 0x00;
-  header[24] = 0x80;
-  header[25] = 0x3E;
-  header[26] = 0x00;
-  header[27] = 0x00;
-  header[28] = 0x00;
-  header[29] = 0x7D;
-  header[30] = 0x00;
-  header[31] = 0x00;
-  header[32] = 0x02;
-  header[33] = 0x00;
-  header[34] = 0x10;
-  header[35] = 0x00;
-  header[36] = 'd';
-  header[37] = 'a';
-  header[38] = 't';
-  header[39] = 'a';
-  header[40] = (byte)(wavSize & 0xFF);
-  header[41] = (byte)((wavSize >> 8) & 0xFF);
-  header[42] = (byte)((wavSize >> 16) & 0xFF);
-  header[43] = (byte)((wavSize >> 24) & 0xFF);
-}
-
 
 void listSPIFFS(void) {
   Serial.println(F("\r\nListing SPIFFS files:"));
